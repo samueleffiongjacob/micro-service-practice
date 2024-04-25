d0c3c3b498e7aacb2d1c9270a9bca929719990ab
samueleffiongjacob <samueleffiong685@gmail.com>
2024-04-25 15:07:55 +0100

    typescript

---
+++ Typescript/fetures/tuple.ts
@@ -0,0 +1,20 @@
+// Tuple
+const drink = {
+    color: 'brown',
+    carbonated: true,
+    sugar: 40
+};
+
+// Type alias
+type Drink = [string, boolean, number];
+
+const pepsi: Drink = ['brown', true, 40];
+
+const sprite: Drink = ['clear', true, 40];
+
+const tea: Drink = ['brown', false, 0];
+
+
+// Tuple
+const carSpecs: [number, number] = [400, 3354];
+
